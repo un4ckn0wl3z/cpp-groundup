bd297e07b85c09997075a3a9e21fe6198fdc4d07
un4ckn0wl3z <slickerth@gmail.com>
2019-12-28 20:51:06 +0700

    add namespace

---
+++ 2_Namespace02/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// this is a comment
+
+/*
+This is
+multiple line
+commment
+*/
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+	int favorite_number; // this is where my favorite number is stored
+
+	cout << "Enter your favorite number between 1 and 100: ";
+	cin >> favorite_number;
+
+	cout << "Amazing!! That my favorite number too!" << endl;
+	cout << "No really!! " << favorite_number << " is my favorite number" << endl;
+
+
+	return 0;
+
+}
