e6ebdd6a2ffea92efc17f2ab131602d747e067a2
un4ckn0wl3z <slickerth@gmail.com>
2019-12-28 20:36:00 +0700

    add comment

--- 0_VeryFirstProgram/Main.cpp
+++ 0_VeryFirstProgram/Main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+// this is a comment
+
+/*  
+This is 
+multiple line 
+commment
+*/
+
 int main()
 {
-	int favorite_number;
+	int favorite_number; // this is where my favorite number is stored
 
 	std::cout << "Enter your favorite number between 1 and 100: ";
 	std::cin >> favorite_number;
